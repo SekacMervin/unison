3f31f6816aa984c348197f6651e8bc7e3e41e186
Daniel Röschel <LK18KN@INSIM.BIZ>
2022-12-21 23:23:21 +0100

    Uprava Layoutu

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,18 +1,41 @@
 
 //// Import backages
 import { Outlet, Link } from "react-router-dom";
-import LayoutStyles from "./Layout.css";
+import Styles from "./Styles.scss";
+
+// Icons
+import logoInzert from '../Images/logo-inzert.png';
+import logoUnison from '../Images/logo-unison.png';
+import { Facebook } from 'react-bootstrap-icons'
+import { Instagram } from "react-bootstrap-icons";
 
 
 const Layout = () => {
     return (
       <>
         <div className="header">
-            <h1>Nadpis</h1>
+          <div className="leftSite">
+            <Link to="/" className="logoLink" title="Unison">
+              <img className="logoImg" src={logoUnison} alt="Logo unison"/>
+            </Link>
+            <Link to="/" className="logoLink" title="Inzert">
+              <img className="logoImg" src={logoInzert} alt="Logo inzert"/>
+            </Link>
+          </div>
+          <div className="middleSite">
+            <h2>INZERTNÍ NOVINY DO KAŽDÉ RODINY</h2>
+          </div>
+          <div className="rightSite">
+            <a className="iconLink" href="https://www.facebook.com" target="_blank" rel="noreferrer" title="Facebook">
+              <Facebook className="iconFacebook" />
+            </a>
+            <a className="iconLink" href="https://www.instagram.com" target="_blank" rel="noreferrer" title="Instagram">
+              <Instagram className="iconInstagram" />
+            </a>
+          </div>
         </div>
         <div className="mainbody">
             <div className="slidebar">
-
             </div>
             <div className="contentBody">
                 <Outlet />
